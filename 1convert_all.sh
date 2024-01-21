c8fa5303b04a90f2dbb395f60afdd875cf2413ab
Jose Antonio Alvarez Cubero <jacubero@users.noreply.github.com>
2024-01-21 22:50:04 +0100

    Delete 1convert_all.sh

--- 1convert_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in $(ls *.{py,txt,gin,in,sh})
-do
-  base="wget https://raw.githubusercontent.com/google-deepmind/alphageometry/main/"
-  command=$base$file
-  echo $command
-done
